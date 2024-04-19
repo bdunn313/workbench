62225ea223a2abdef6becd077ad44ddf1b51424d
Brad Dunn <brad@braddunn.com>
2024-04-18 22:48:50 -0400

    fix: make base zx command do something

--- cmd/zx.go
+++ cmd/zx.go
@@ -4,23 +4,15 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 // zxCmd represents the zx command
 var zxCmd = &cobra.Command{
 	Use:   "zx",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Run a script via zxjs. Can target markdown files and mjs files.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("zx called")
+		cmd.Help()
 	},
 }
 
