dcb10e7858c7cea6e71fa8ba3e170dfa357bf8ea
Brad Dunn <brad@braddunn.com>
2024-11-12 20:00:21 -0500

    docs: fix default boilerplate for cobra commands
    Fixes #6

--- cmd/root.go
+++ cmd/root.go
@@ -31,16 +31,9 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "workbench",
 	Version: "0.2.1",
-	Short:   "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short:   "A collection of personal tools and knick-knacks.",
+	Long: `Workbench is a collection of personal tools and knick-knacks that I
+	add to as I find things that I need to automate or simplify.`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
